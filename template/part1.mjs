e6590c213c26f9d821e478eedc059e273d372ac7
Henry Lau <henrylaush@gmail.com>
2021-12-08 22:02:40 +0800

    add utils to template, js lacks array funcs

--- template/part1.mjs
+++ template/part1.mjs
@@ -8,6 +8,25 @@ const lines = [];
 function processLine(line) {
     lines.push(line.split(',').map(Number));
 }
+
+// utils
+Object.defineProperties(Array.prototype, {
+  sum: {
+    value: function(getValue = id => id) { return this.reduce((partial, item) => partial + getValue(item), 0)}
+  },
+  all: {
+    value: function(getValue = id => id) { return this.reduce((partial, item) => partial && !!getValue(item), true)}
+  },
+  any: {
+    value: function(getValue = id => id) { return this.reduce((partial, item) => partial || !!getValue(item), false)}
+  },
+  gather: {
+    value: function(getKey = id => id, combiner = (existing, value) => ([...(existing ?? []), value])) { 
+      return this.map(item => [getKey(item), item]).reduce((bag, [key, value]) => ({ ...bag, [key]: combiner(bag[key], value) }), {})
+    }
+  }
+})
+
 // End template, start code
 function processLines(lines) {
 
