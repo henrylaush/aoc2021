feb4ea848a27629f1c37e29e30dd0bac4b5c50f6
Henry Lau <henrylaush@gmail.com>
2021-12-20 17:50:24 +0800

    add day 18, 20

---
+++ Day18/part2.mjs
@@ -0,0 +1,129 @@
+import { createInterface } from 'readline';
+import '../utils.mjs';
+import { compose } from '../utils.mjs';
+
+const rl = createInterface({
+  input: process.stdin
+});
+
+const lines = [];
+
+function processLine(line) {
+    lines.push(line);
+}
+
+// End template, start code
+
+function asList(line) {
+  const list = [];
+  let level = 0;
+  for (let c of line) {
+    if( c === '[') { level++ }
+    else if (c === ']') { level-- }
+    else if (c === ',') { continue }
+    else {
+      list.push({ level, value: Number(c) });
+    }
+  }
+  return list
+}
+
+function explode(list) {
+  let cur = 0;
+  while (cur < list.length - 1) {
+    if (list[cur].level === 5 && list[cur + 1].level === 5) {
+      const left = list[cur - 1];
+      const p1 = list[cur];
+      const p2 = list[cur + 1];
+      const right = list[cur + 2];
+      if (left) {
+        left.value += p1.value
+      }
+      if (right) {
+        right.value += p2.value
+      }
+      p1.level -= 1;
+      p1.value = 0;
+      list.splice(cur + 1, 1);
+      return { didSomething: true, list };
+    }
+    cur++
+  }
+  return { didSomething: false, list };
+}
+
+function split(list) {
+  return list.reduce((p, data) => (p.didSomething || data.value < 10 ) ? { 
+    didSomething: p.didSomething,
+    list: [...p.list, data],
+  } : {
+    didSomething: true,
+    list: [
+      ...p.list,
+      {level: data.level + 1, value: Math.floor(data.value / 2)},
+      {level: data.level + 1, value: Math.ceil(data.value / 2)},
+    ]
+  }, { didSomething: false, list: [] })
+}
+
+function add(l1, l2) {
+  return [...l1, ...l2].map(data => ({ level: data.level + 1, value: data.value }));
+}
+
+function resolve(list) {
+  let didSomething = true;
+  let result = list;
+  while (didSomething) {
+    const exResult = explode(result);
+    result = exResult.list;
+    didSomething = exResult.didSomething;
+
+    if (didSomething) { 
+      // console.log('after explode: ', result); 
+      continue 
+    }
+    
+    const splitResult = split(result);
+    result = splitResult.list;
+    didSomething = splitResult.didSomething;
+    // if (didSomething) { console.log('after split: ', result);}
+  }
+  return result
+}
+
+function flatten(list) {
+  while(list.length !== 1) {
+    let i = 0;
+    while(true) {
+      if (list[i].level === list[i+1]?.level) {
+        list[i].value = 3* list[i].value  + 2 * list[i+1].value;
+        list[i].level -= 1;
+        list.splice(i + 1, 1);
+        break;
+      }
+      i++
+    }
+  }
+  return list[0].value;
+}
+
+function processLines(lines) {
+  const pairs = [];
+  for (let p1 of lines) {
+    for (let p2 of lines) {
+      if (p1 !== p2) {
+        pairs.push([p1, p2].map(asList));
+      }
+    }
+  }
+
+  return Math.max(...pairs.map(([p1, p2]) => compose(resolve)(add)(p1, p2)).map(flatten))
+}
+
+// End code, start template
+function handleProblem() {
+    console.log(processLines(lines));
+    process.exit(0);
+}
+
+rl.on('line', processLine).on('close', handleProblem);
---
+++ Day20/part1.mjs
@@ -0,0 +1,74 @@
+import { createInterface } from 'readline';
+import '../utils.mjs';
+
+const rl = createInterface({
+  input: process.stdin
+});
+
+const state = {
+  table: [],
+  image: [],
+};
+
+function processLine(line) {
+  if (line.length === 512) {
+    state.table = Array.from(line).map(c => c === '.' ? 0 : 1);
+  } else if (line.length) {
+    state.image.push(Array.from(line).map(c => c === '.' ? 0 : 1));
+  }
+}
+
+// End template, start code
+function printImage(image) {
+  console.log(image.map(row => row.map(v => v === 0 ? '.': '#').join('')).join('\n'));
+}
+
+function padImage(image, emptyAs) {
+  const empty = Array(image[0].length + 4).fill(emptyAs);
+  return [
+    empty,
+    empty,
+    ...image.map(row => [emptyAs, emptyAs, ...row, emptyAs, emptyAs]), 
+    empty,
+    empty,
+  ]
+}
+
+function getNeigbours(x, y) {
+  return [
+    [x - 1, y - 1],
+    [x - 1, y    ],
+    [x - 1, y + 1],
+    [x    , y - 1],
+    [x    , y    ],
+    [x    , y + 1],
+    [x + 1, y - 1],
+    [x + 1, y    ],
+    [x + 1, y + 1],
+  ]
+}
+
+function getValueFrom(image, table, indices, emptyAs) {
+  const idx = indices.reduce((p, [x,y]) => ((p << 1) + (image[x]?.[y] ?? emptyAs)), 0);
+  return table[idx];
+}
+
+function convolution(image, table, emptyAs) {
+  return image.map((row, x) => (
+    row.map((_, y) => getValueFrom(image, table, getNeigbours(x,y), emptyAs))
+  ));
+}
+
+function processLines({ table, image }) {
+  const once = convolution(padImage(image, 0), table, 0);
+  const twice = convolution(padImage(once, table[0]), table, table[0]);
+  return twice.flatMap(id => id).filter(Boolean).length;
+}
+
+// End code, start template
+function handleProblem() {
+    console.log(processLines(state));
+    process.exit(0);
+}
+
+rl.on('line', processLine).on('close', handleProblem);
